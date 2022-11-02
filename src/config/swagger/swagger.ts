cbb08f88091807956a1e37d04bb5b58c91281609
kai <hieu.le@dedoco.com>
2022-11-02 17:10:55 +0800

    reduce affect surface group controller

--- src/config/swagger/swagger.ts
+++ src/config/swagger/swagger.ts
@@ -1,13 +1,11 @@
 import * as fs from 'fs';
 import { INestApplication, PipeTransform } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { SwaggerExplorer } from '@nestjs/swagger/dist/swagger-explorer';
+import { PATH_METADATA, PIPES_METADATA } from '@nestjs/common/constants';
 import { DECORATORS } from '@nestjs/swagger/dist/constants';
 import * as ApiParametersExplorer from '@nestjs/swagger/dist/explorers/api-parameters.explorer';
-import {
-  ParameterMetadataAccessor,
-  ParamWithTypeMetadata,
-} from '@nestjs/swagger/dist/services/parameter-metadata-accessor';
+import * as ApiUseTagsExplorer from '@nestjs/swagger/dist/explorers/api-use-tags.explorer';
+import { ParameterMetadataAccessor } from '@nestjs/swagger/dist/services/parameter-metadata-accessor';
 import {
   ParameterLocation,
   ParameterObject,
@@ -15,10 +13,9 @@ import {
 } from '@nestjs/swagger/dist/interfaces/open-api-spec.interface';
 import { patchNestJsSwagger, zodToOpenAPI } from 'nestjs-zod';
 import { isZodDto } from 'nestjs-zod/dto';
-import { JoiValidationPipe } from '../../common/joi-pipe';
-import joiToSwagger from 'joi-to-swagger';
 import { ObjectSchema } from 'joi';
-import { PIPES_METADATA } from '@nestjs/common/constants';
+import joiToSwagger from 'joi-to-swagger';
+import { JoiValidationPipe } from '../../common/joi-pipe';
 
 function setupSwagger(app: INestApplication) {
   const path = process.env.API_PREFIX || '/api';
@@ -51,65 +48,84 @@ function setupSwagger(app: INestApplication) {
   SwaggerModule.setup(path, app, document);
 }
 
+/**
+ * Group endpoint by controller path. As method are initialised before class,
+ * register callbacks to add ApiTags to each endpoint.
+ *
+ * Alternative: SwaggerExplorer.exploreGlobalMetadata; less code but more hacky
+ * instance[prop] = prototype[prop]; prototype[prop] = () => instance[prop]
+ * */
 const groupControllersByPath = () => {
-  const instance = SwaggerExplorer as any;
-  const method = 'exploreGlobalMetadata';
+  const _instance = ApiUseTagsExplorer as any;
+  const KEY = '__group_path_swagger__';
 
-  // override instance to contextualise `this`
-  instance[method] = instance.prototype[method];
-  // override prototype to add custom behaviour
-  instance.prototype[method] = (metatype: any) => {
-    if (!Reflect.hasMetadata(DECORATORS.API_TAGS, metatype)) {
-      let metadata = Reflect.getMetadata('path', metatype);
-      metadata = metadata[0] === '/' ? metadata : `/${metadata}`;
-      Reflect.defineMetadata(DECORATORS.API_TAGS, [metadata], metatype);
+  // Method override, register callbacks
+  const _propM = 'exploreApiTagsMetadata';
+  const _superM = _instance[_propM];
+  _instance[_propM] = (instance: any, prototype: any, method: any) => {
+    let cbs = Reflect.getMetadata(KEY, prototype);
+    if (!cbs) {
+      cbs = [];
+      Reflect.defineMetadata(KEY, cbs, prototype);
     }
-    return instance[method](metatype);
+    cbs.push((metatype: any) => {
+      // add tag to group endpoint
+      let metadata = Reflect.getMetadata(PATH_METADATA, metatype);
+      metadata = metadata[0] === '/' ? metadata.substring(1) : metadata;
+      Reflect.defineMetadata(DECORATORS.API_TAGS, [metadata], metatype);
+    });
+    return _superM(instance, prototype, method);
+  };
+
+  // Class override, execute callbacks
+  const _propC = 'exploreGlobalApiTagsMetadata';
+  const _superC = _instance[_propC];
+  _instance[_propC] = (metatype: any) => {
+    const cbs: any[] = Reflect.getMetadata(KEY, metatype.prototype);
+    if (cbs) {
+      for (const cb of cbs) cb(metatype);
+      Reflect.deleteMetadata(KEY, metatype.prototype);
+    }
+    return _superC(metatype);
   };
 };
 
 const validateQueryAndParam = () => {
   const _instance = ApiParametersExplorer as any;
-  const _method = 'exploreApiParametersMetadata';
 
+  const _prop = 'exploreApiParametersMetadata';
+  const _super = _instance[_prop];
   const _accessor = new ParameterMetadataAccessor();
-  const t = _instance[_method];
-  _instance[_method] = (
+  _instance[_prop] = (
     schemas: any,
     instance: any,
     prototype: any,
     method: any,
   ) => {
+    // add param and query validation to swagger
     Object.values(_accessor.explore(instance, prototype, method) || {})
       .filter((obj) => obj.in !== 'body')
-      .map((metadata) => transformMetadataToParams(metadata, method))
+      .map((metadata): ParameterObject[] => {
+        const schemaObject = getSchemaObject(metadata, method);
+        if (!schemaObject) return [];
+
+        const required = new Set(schemaObject.required);
+        return Object.entries(
+          schemaObject.properties as { [_: string]: SchemaObject },
+        ).map(([name, { type }]) => ({
+          name,
+          type,
+          in: metadata.in as ParameterLocation,
+          required: required.has(name),
+        }));
+      })
       .filter((params) => params)
       .forEach((params) => {
         Reflect.defineMetadata(DECORATORS.API_PARAMETERS, params, method);
       });
-    return t(schemas, instance, prototype, method);
+    return _super(schemas, instance, prototype, method);
   };
 };
-
-function transformMetadataToParams(
-  metadata: ParamWithTypeMetadata,
-  method: any,
-): ParameterObject[] {
-  const sObj = getSchemaObject(metadata, method);
-  if (!sObj) return [];
-
-  const inType = metadata.in as ParameterLocation;
-  const properties = sObj.properties as { [_: string]: SchemaObject };
-  const required = new Set(sObj.required);
-
-  return Object.entries(properties).map(([name, { type }]) => ({
-    name,
-    type,
-    in: inType,
-    required: required.has(name),
-  }));
-}
-
 //-- Validator-specific --//
 
 function getSchemaObject(metadata: any, method: any): SchemaObject | undefined {

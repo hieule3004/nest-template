e7505de9c2becb4530d45612f758f151822267a3
kai <hieu.le@dedoco.com>
2022-11-11 11:28:06 +0800

    cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggingModule } from './config/logging/logging.module';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './user/user.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { GlobalValidationProvider } from './config/validation';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    MongooseModule.forRootAsync({
-      useFactory: () => ({
-        uri: process.env.MONGO_URL,
-      }),
-    }),
-    UserModule,
-    LoggingModule,
-  ],
+  imports: [ConfigModule.forRoot(), LoggingModule],
   controllers: [AppController],
   providers: [GlobalValidationProvider, AppService],
 })

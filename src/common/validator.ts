5eb488626fe001b3626416f725ffba757cdd7a43
kai <hieu.le@dedoco.com>
2022-11-14 16:00:07 +0800

    cleanup

--- src/common/validator.ts
+++ src/common/validator.ts
@@ -1,59 +1,79 @@
-import * as https from 'https';
 import * as fs from 'fs';
 import * as Heket from 'heket';
-
-//---- Struct ----//
+import * as https from 'https';
 
 const { RuleNotFoundError } = Heket as any;
 
-const ABNF_URL = 'https://author-tools.ietf.org/api/abnf/extract';
+//---- Definition ----//
+
 const RESOURCE_DIR = 'dist/resources';
 
-const rfc: { [docId: number]: Heket.Parser } = {};
+/** add validator here with rfc document id and rule to parse */
+const ValidatorType = {
+  email: { docId: 5322, ruleName: 'addr-spec' },
+  phone: { docId: 3966, ruleName: 'telephone-subscriber' },
+  iso8601DateTime: { docId: 3339, ruleName: 'date-time' },
+};
 
-//---- Static init ----//
+type ValidatorKey = keyof typeof ValidatorType;
+const rfc: Partial<Record<ValidatorKey, Heket.Parser>> = {};
 
 /** read rfc specification */
-{
-  registerValidator(3966, 'telephone-subscriber');
-  registerValidator(5322, 'addr-spec');
-  registerValidator(3339, 'date-time');
-}
+Object.entries(ValidatorType).forEach(([type, { docId, ruleName }]) =>
+  _registerValidator(type as ValidatorKey, docId, ruleName),
+);
 
 //---- Validator ----//
+/** export validator here */
 
 export const emailValidator = (raw: string): boolean =>
-  !!rfc[5322].parseSafe(raw);
+  !!rfc.email?.parseSafe(raw);
 
 export const phoneValidator = (raw: string): boolean =>
-  !!rfc[3966].parseSafe(raw);
+  !!rfc.phone?.parseSafe(raw);
 
 export const iso8601DateValidator = (raw: string): boolean =>
-  !!rfc[3339].parseSafe(raw);
+  !!rfc.iso8601DateTime?.parseSafe(raw);
 
-//---- Helper function ----//
+//---- Helper ----//
+
+const ABNF_URL = 'https://author-tools.ietf.org/api/abnf/extract';
 
 /** Helper function to create parser */
-function registerValidator(id: number, ruleName: string): void {
-  try {
-    const clientRequest = https.get(`${ABNF_URL}?doc=${id}`, (res) => {
-      let abnf = '';
-      res.on('data', (chunk) => (abnf += chunk.toString()));
-      res.on('end', () => _addParser(id, ruleName, abnf));
-    });
-    clientRequest.end();
-  } catch (e) {
-    // use offline version in case of connection issue
-    fs.readFile(
-      `${RESOURCE_DIR}/${id}.abnf`,
-      { encoding: 'utf-8' },
-      (_, abnf) => _addParser(id, ruleName, abnf),
-    );
-  }
+function _registerValidator(
+  type: ValidatorKey,
+  id: number,
+  ruleName: string,
+): void {
+  // read offline spec
+  fs.readFile(
+    `${RESOURCE_DIR}/${id}.abnf`,
+    { encoding: 'utf-8' },
+    (_, abnf) => {
+      if (abnf) {
+        _addParser(type, id, ruleName, abnf);
+        return;
+      }
+      // read online spec if failed
+      https
+        .get(`${ABNF_URL}?doc=${id}`, (res) => {
+          let abnf = '';
+          res.on('data', (chunk) => (abnf += chunk.toString()));
+          res.on('end', () => _addParser(type, id, ruleName, abnf));
+        })
+        .on('error', () => undefined)
+        .end();
+    },
+  );
 }
 
-/** Add parser to {@link rfc} map */
-function _addParser(id: number, ruleName: string, abnf: string): void {
+/** Add parser to RFC map */
+function _addParser(
+  type: ValidatorKey,
+  id: number,
+  ruleName: string,
+  abnf: string,
+): void {
   // fix spacing of extracted document to follow ABNF format
   abnf = abnf.split('\n').reduce((a, s) => {
     // remove comments
@@ -63,7 +83,7 @@ function _addParser(id: number, ruleName: string, abnf: string): void {
     // add new rule
     if (s.match(/^[A-Za-z0-9-\s]+=[/\s]+/)) a += '\n\n' + s;
     // join multiline rule
-    else if (a[a.length - 1].match(/[=/]/)) a += s;
+    else if (a[a.length - 1]?.match(/[=/]/)) a += s;
     return a;
   }, '');
 
@@ -84,7 +104,13 @@ function _addParser(id: number, ruleName: string, abnf: string): void {
   }
 
   // save to local to be used if needed
-  fs.writeFile(`${RESOURCE_DIR}/${id}.abnf`, abnf, () => undefined);
+  fs.mkdirSync(RESOURCE_DIR, { recursive: true });
+  fs.writeFile(
+    `${RESOURCE_DIR}/${id}.abnf`,
+    abnf,
+    { encoding: 'utf-8' },
+    () => undefined,
+  );
   // add parser to cache
-  rfc[id] = Heket.createParser(rules.getRuleByName(ruleName), rules);
+  rfc[type] = Heket.createParser(rules.getRuleByName(ruleName), rules);
 }
